8ea25e02e4be99ed99d803b2436c6637b39f4b0c
Nguyencoder11 <lenhnguyen10a22003@gmail.com>
2024-10-07 13:52:13 +0700

    First Commit

---
+++ server/msplayer/src/main/java/com/app/msplayer/MsplayerApplication.java
@@ -0,0 +1,13 @@
+package com.app.msplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsplayerApplication.class, args);
+	}
+
+}
---
+++ server/msplayer/src/test/java/com/app/msplayer/MsplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.msplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
